5e7a902dbf8c2a426c9a5cd1017c196b257fa783
Jordan Hickman <jdhickman816@outlook.com>
2021-01-30 19:24:15 -0600

    commit commit

---
+++ CSF1Homework/FruitLoops1and2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoops1and2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Fruit Loops 1");
+            Console.Title = "Fruit Loops 1";
+
+
+            //Declare and initialize an array of any six fruit names (as text). Set up a FOR loop and iterate through them (displaying each value
+            //in the console.)
+            string[] fruits = new string[6]; //declare
+            fruits[0] = "banana";
+            fruits[1] = "apple";
+            fruits[2] = "orange";
+            fruits[3] = "grapefruit";
+            fruits[4] = "strawberry";
+            fruits[5] = "blueberry";
+
+
+
+
+            Console.WriteLine("Here are our fruit selections!");
+
+            string index = "";
+            foreach (string fruit in fruits)
+            {
+               
+                Console.WriteLine(fruit);
+                index += ".";
+            }
+            Console.WriteLine(index);
+
+
+
+        }
+    }
+}
